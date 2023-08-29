7373612658cd492ca1759ef813c133e67f1773ed
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2023-08-29 12:36:19 -0500

    tryng render url for 100th time

--- app/client/src/config.js
+++ app/client/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://blog-app-wvlc.onrender.com/";
+const BASE_URL = "https://blog-app-wvlc.onrender.com";
 
 export const axiosInstance = axios.create({ baseURL : BASE_URL });
 
